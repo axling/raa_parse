ed134f56d944571f6cc8497d1dc7ecc6b87be3f2
Erik Axling <axling@gmail.com>
2012-04-13 23:09:10 +0200

    Add support for getting all ids for one county

--- raa_parse/crawl.py
+++ raa_parse/crawl.py
@@ -8,15 +8,24 @@ url = "http://www.fmis.raa.se/cocoon/fornsok/ajax/searchresults"
 
 user_agent = 'Mozilla/5 (Solaris 10) Gecko'
 headers = {'User-Agent' : user_agent}
-
-values = {'county': '05', 'page': 1}
-data = urllib.urlencode(values)
-
-req = urllib2.Request(url, data, headers)
-response = urllib2.urlopen(req)
-soup = BeautifulSoup(response.read())
-all_a = soup.table.find_all("a")
 ids = []
-for tag in all_a:
-    ids.append(tag['id'])
-print ids
+start_page=1
+while True:
+    values = {'county': '05', 'page': start_page}
+    data = urllib.urlencode(values)
+
+    req = urllib2.Request(url, data, headers)
+    response = urllib2.urlopen(req)
+    soup = BeautifulSoup(response.read())
+    if soup.p != None:
+        break
+    all_a = soup.table.find_all("a")
+    
+    for tag in all_a:
+        ids.append(tag['id'])
+    
+    print "page " + str(start_page) + " completed"
+    print "ids: " + str(len(ids)) 
+    start_page = start_page + 1
+        
+print len(ids)
