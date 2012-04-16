754ca9b6d05f239437954e7c82b13566b7bc31ca
unknown <eeriaxl@EVB499BAE10343.eemea.ericsson.se>
2012-04-16 13:22:17 +0200

    updates

--- crawl.py
+++ crawl.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import urllib
 import urllib2
 import string
+import json
+import re
+import httplib
+import requests
 
 url = "http://www.fmis.raa.se/cocoon/fornsok/ajax/searchresults"
 
@@ -10,6 +14,72 @@ user_agent = 'Mozilla/5 (Solaris 10) Gecko'
 headers = {'User-Agent' : user_agent}
 ids = []
 start_page=1
+
+def put_in_database(doc):
+    url = "http://localhost.com:5984"
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+    r = requests.get(url + "/_uuids")
+    print r.text
+    uuid_dict = json.loads(r.text)
+    uuid = uuid_dict["uuids"].index(0)    
+
+    header = {'content-type': 'application/json'}
+    r = request.put(url + "/remains/" + uuid, data=doc, headers=header)
+            
+    response_dict = json.loads(r.text)
+    response_dict['ok'] = "true"
+    
+
+def get_remain_page(remain_id):
+    url = "http://www.fmis.raa.se/cocoon/fornsok/ajax/showobject"
+    values = {'objektid': remain_id}
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data, headers)
+    
+    response = urllib2.urlopen(req)
+    soup = BeautifulSoup(response.read())
+    raa_num = soup.table['summary']
+    trs = soup.find_all("tr")
+    doc = {}
+    for tr in trs:
+        if not tr.th:
+            continue
+        if 'class' in tr.attrs:
+            if tr['class'] == "rowheader" or tr['class'] == "rowsub" or tr['class'] == "rowsubheader":
+                continue
+        elif 'id' in tr.th.attrs:
+            if tr.th['id'] == "remnpopanchor":
+                link = tr.td.a['href']
+                name = tr.th.string
+                value = tr.td.a.string
+                doc[name] = {"http_link": link, "text": value}
+                
+            elif tr.th['id'] == "Antikvarisk bed&ouml;mning_anchor":
+                link = tr.td.a['href']
+                name = tr.th.string
+                value = tr.td.a.string
+                doc[name] = {"http_link": link, "text": value}
+            else:
+                name = tr.th.string
+                value = tr.td.string
+                doc[name] = value        
+        else:
+            if tr.th.find(text=re.compile("Google Earth")) != None:
+                pass
+            elif tr.th.find(text=re.compile("eniro.se")) != None:
+                pass
+            elif tr.th.find(text=re.compile("hitta.se")) != None:
+                pass
+            else:
+                name = tr.th.string
+                value = tr.td.string
+                doc[name] = value
+    return json.dumps(doc)
+            
+
+return_doc = get_remain_page("18000000050627")
+put_in_database(return_doc)
+
 while True:
     values = {'county': '05', 'page': start_page}
     data = urllib.urlencode(values)
@@ -29,3 +99,5 @@ while True:
     start_page = start_page + 1
         
 print len(ids)
+
+
